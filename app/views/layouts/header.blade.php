a221ebf24a5a5d5c03e50077aeb70c5a7a84f353
ujacha <lemite4u@daum.net>
2015-01-07 15:35:22 +0900

    apply .env.example

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -5,7 +5,7 @@
         <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
     </div>
     <!--logo start-->
-    <a href="{{ url('/') }}" class="logo"><b>MY BOOK</b></a>
+    <a href="{{ url('/') }}" class="logo"><b>{{ getenv('APP_NAME'); }}</b></a>
     <!--logo end-->
     <div class="nav notify-row" id="top_menu">
         <!--  notification start -->
